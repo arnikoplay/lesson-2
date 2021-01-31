69b63a382fa30dbbe31e743675d69b75c6438bbd
arnikoplay <77569009+arnikoplay@users.noreply.github.com>
2021-01-31 13:48:20 +0300

    all normal

--- server/index.js
+++ server/index.js
@@ -1,1 +1,13 @@
-index.js
+let express = require("express");
+let app = express();
+
+app.get("/", function (req, res) {
+	res.send("hello, Node.js!");
+});
+
+app.get("/test", function(req, res){
+	let x = req.query.x;
+	response.send("you send " + x + "for me");
+})
+
+app.listen(8080);
